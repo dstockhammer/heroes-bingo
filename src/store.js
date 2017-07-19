74a723c6774f1310da9863527071d90a18dc5714
Daniel Stockhammer <daniel@stockhammer.it>
2017-07-19 19:46:23 +0100

    rename eu 2017 file and add some new phrases (thx reddit)

--- src/store.js
+++ src/store.js
@@ -4,9 +4,9 @@ export default {
     current: null,
     boards: {},
     sets: {
-      selected: { key: 'hgc-eu-phase2-2017', text: 'HGC Europe 2017 Phase 2' },
+      selected: { key: 'hgc-eu-2017-phase2', text: 'HGC Europe 2017 Phase 2' },
       available: [
-        { key: 'hgc-eu-phase2-2017', text: 'HGC Europe 2017 Phase 2' },
+        { key: 'hgc-eu-2017-phase2', text: 'HGC Europe 2017 Phase 2' },
         { key: 'heroes-bingo-2015', text: 'Heroes Bingo 2015' },
       ],
     },
